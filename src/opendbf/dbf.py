5f9722f29c2921f45fa0045e4c18fda15dda4d70
Fangchen Li <fangchen.li@outlook.com>
2024-09-09 18:38:00 -0700

    Check header terminator

--- src/opendbf/dbf.py
+++ src/opendbf/dbf.py
@@ -22,17 +22,15 @@ def dbf_to_csv(file_path: str | PathLike) -> Path:
         field_list = [Field(file, header.encoding) for _ in range(num_fields)]
         field_name = [field.field_name for field in field_list]
 
+        assert file.read(2) == b"\r ", "Header terminator not found."
+
         with open(csv_path, "w", newline="", encoding="utf-8") as csv_file:
             csv_writer = writer(csv_file, delimiter=",", quoting=QUOTE_MINIMAL)
             csv_writer.writerow(field_name)
-            
+
             for _ in range(header.num_records):
                 record = []
                 for field in field_list:
-
-                    if file.peek(1)[:1] in [b"\r", b"\n"]:
-                        file.seek(2, SEEK_CUR)
-
                     data_bytes = file.read(field.field_length)
                     data_bytes = data_bytes.strip()
                     data_str = data_bytes.decode(header.encoding)
